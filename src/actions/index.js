0f3163a812f1ae063b2dfecaceea477da265332d
Nick Arora <arora.nick@gmail.com>
2016-01-19 15:43:38 -0800

    use ActiveBookReducer to handle the BOOK_SELECTED action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
 export function selectBook(book) {
-    console.log('A book has been selected: ', book.title);
+    // selectBook is an actioncreator
+    // therefore it must return an action
+    // an action is an object with a 'type' property
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBookReducer from './reducer_active_book';
 
 // mapping of our state!
 // we never want more than one store in our app
 // therefore, to keep concerns seperated, we use combineReducers to create a single root reducer
 const rootReducer = combineReducers({
 
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBookReducer
 
 });
 
